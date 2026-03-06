a7ac3772d3b25817e067fc1a15c7c2eb0a113f13
jaehee322 <jaehee322@naver.com>
2026-03-07 01:10:16 +0900

    why build error

--- build.sh
+++ build.sh
@@ -5,10 +5,6 @@ set -o errexit
 echo "Installing requirements..."
 pip install -r requirements.txt
 
-echo "Fixing Alembic Migration Sync Issue..."
-# Render DB에 Alembic 버전이 없을 경우를 대비한 stamp (이미 찍혀있다면 무시됨)
-# 현재 Render DB에 적용 완료된 최신 리비전으로 도장
-flask db stamp 0e6a82b8f6f3 || true
 
 # 새로운 마이그레이션(변경사항)들을 적용
 flask db upgrade
